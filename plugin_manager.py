bfa0e3da5cce5d1c190a7afdcd2458fba1cf0fcc
cynic <kurisufag1@gmail.com>
2022-01-06 11:07:46 -0500

    hot reloading and authentication

--- plugin_manager.py
+++ plugin_manager.py
@@ -7,12 +7,21 @@ class plugin_manager():
         plugins = [name.split(".py")[0] for name in os.listdir(plugin_path) if name.endswith(".py")]
         self.plugins_loaded = []
         for plugin in plugins:
-            module = __import__(plugin)
+            try:
+                module = __import__(plugin)
+            except: 
+                logger.write(f"error loading plugin {plugin}")
+                continue
             if not(hasattr(module, "run")) or not(hasattr(module, "hooks")):
-                self.logger.write("plugin {plugin} is invalid: crucial attribute missing!")
+                logger.write(f"plugin {plugin} is invalid: crucial attribute missing!")
+                continue
             else:
                 self.plugins_loaded.append(module)
+                logger.write(f"loaded plugin {module.__name__}") # this name will be used for hotloading
     def handle(self, event, ctx, bot):
         for plugin in self.plugins_loaded:
             if event in plugin.hooks:
-                getattr(plugin, "run")(event, ctx, bot)
+                try:
+                    getattr(plugin, "run")(event, ctx, bot)
+                except Exception as e:
+                    bot.logger.write(e)
